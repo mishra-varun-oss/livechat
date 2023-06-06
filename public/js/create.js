192f5d4c9b84d4ebc0e591b1e62376a8b12ea915
root <root@ip-172-31-80-169.ec2.internal>
2023-06-06 15:23:32 +0000

    Added pop up window functionality, and implemented URL logic.

--- public/js/create.js
+++ public/js/create.js
@@ -1,4 +1,7 @@
 let form = document.querySelector("#create_chat_form");
+let chat_enter = document.querySelector("#chat_enter");
+
+chat_enter.style.display = "none";
 
 async function post(url = "", data = {}) {
 	const response = await fetch(url, { 
@@ -20,9 +23,22 @@ form.addEventListener("submit", (e) => {
 	post("https://laalwood.com/create", { name: host_name, password: password })
 	.then((data) => {
 		if (data.status) {
+			let uid = data.uid;
+			chat_enter.setAttribute("data-value", uid);
+			chat_enter.style.display = "block";
 			console.log(`chat opened with id ${data.uid}`);
 		} else {
 			console.log('something went wrong :(');
 		}
 	})
+})
+
+chat_enter.addEventListener("click", (e) => {
+	let uid = chat_enter.getAttribute("data-value");
+	let width = 300;
+	let height = 500;
+	let left = (window.innerWidth / 2) - (width / 2);
+	let top = (window.innerHeight / 2) - (height / 2);
+	let window_style = `width=${width},height=${height},top=${top},left=${left},resizeable=no,scrollbars=no`;
+	window.open(`https://laalwood.com/chat/${uid}`, "Chat", window_style);
 })
